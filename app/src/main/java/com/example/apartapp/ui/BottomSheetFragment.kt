8b4818c0a962e4f3faa473cb1aff87cfc5e0261f
Ruslan <screepya@gmail.com>
2024-04-05 16:20:22 +0300

    button troubles

---
+++ app/src/main/java/com/example/apartapp/ui/BottomSheetFragment.kt
@@ -0,0 +1,17 @@
+package com.example.apartapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment: BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
